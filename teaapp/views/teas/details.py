81f48ded86cc6e7be6011f62eb6e22f551b6ac96
Matthew Baker <mattbaker2281@gmail.com>
2020-05-17 15:35:36 -0500

    added details functionality, started working on delete buttons for details

--- teaapp/views/teas/details.py
+++ teaapp/views/teas/details.py
@@ -1,7 +1,7 @@
 import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from teaapp.models import Tea
+from teaapp.models import Tea, TeaPackaging, Packaging
 from ..connection import Connection
 
 def get_tea(tea_id):
@@ -19,7 +19,7 @@ def get_tea(tea_id):
             tp.packaging_id,
             tp.tea_id,
             p.id pack_id,
-            p.name,
+            p.name packaging_method,
             p.handmade,
             p.production_location
         FROM teaapp_tea t
@@ -28,7 +28,18 @@ def get_tea(tea_id):
         WHERE t.id = ?;
         """, (tea_id,))
 
-        return db_cursor.fetchone()
+        all_teas = db_cursor.fetchall()
+        tea_groups = {}
+        tea_groups_values = tea_groups.values()
+
+        for (tea, packaging) in all_teas:
+            if tea.id not in tea_groups:
+                tea_groups[tea.id] = tea
+                tea_groups[tea.id].packaging_methods.append(packaging)
+            else:
+                tea_groups[tea.id].packaging_methods.append(packaging)
+
+        return tea_groups_values
 
 def create_tea(cursor, row):
     _row = sqlite3.Row(cursor, row)
@@ -38,15 +49,36 @@ def create_tea(cursor, row):
     tea.name = _row['name']
     tea.flavor = _row['flavor']
 
-    return tea
+    tea.packaging_methods = []
+
+    packaging = Packaging()
+    packaging.id = _row['packaging_id']
+    packaging.name = _row['packaging_method']
+    packaging.longevity = _row['longevity_in_months']
+
+    return (tea, packaging)
 
 def tea_details(request, tea_id):
     if request.method == 'GET':
-        tea = get_tea(tea_id)
+        tea_values = get_tea(tea_id)
 
         template = 'teas/details.html'
         context = {
-            'tea': tea
+            'tea_values': tea_values
         }
 
         return render(request, template, context)
+    
+    elif request.method == "POST":
+        form_data = request.POST
+
+        if("actual_method" in form_data and form_data["actual_method"] == "DELETE"):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                    DELETE FROM teaapp_teapackaging
+                    WHERE tp.id = ?
+                """, (tea_id,))
+
+            return redirect(reverse('teaapp:tea_list'))
